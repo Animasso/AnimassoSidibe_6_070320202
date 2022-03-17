28aadeff6fb6c63a10e1a871a73f2a0fc708f554
Animasso Sidibe <asidibe017@gmail.com>
2022-03-17 15:49:39 +0100

    commit

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,11 +1,50 @@
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-	    const response = await fetch("../data/photographers.json")
-        const photographers = response
+	    const data = await fetch("../data/photographers.json")
+        const photographers = data
         // et bien retourner le tableau photographers seulement une fois
         return ({
             photographers: [photographers]})
     }
+    class photographer {
+        constructor(data) {
+            this.name = data.name
+            this.id = data.id
+            this.city = data.city
+            this.country = data.country
+            this.tagline = data.tagline
+            this.price = data.price  
+            this.portait = data.portait           
+        }
+     
+        get name() {
+            return this.name
+        }
+     
+        get id() {
+            return this.id
+        }
+
+        get city() {
+            return this.city
+        }
+
+        get country() {
+            return this.country
+        }
+
+        get tagline() {
+            return this.tagline
+        }
+
+        get price() {
+            return this.price
+        }
+
+        get portait() {
+            return this.portait
+        }
+     }
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
