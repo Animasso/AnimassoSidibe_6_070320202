a6cedffdd0b66cde9bf01d33e94ae00d33a29408
Animasso Sidibe <asidibe017@gmail.com>
2022-03-28 21:31:16 +0200

    responsive

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,12 +7,18 @@ function photographerFactory(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt",name)
+        img.setAttribute("aria-label",'aller vers la page personnel de' + name)
         const link = document.createElement('a')
         link.setAttribute('href',"./photographer.html?id=" + id);
         const h2 = document.createElement( 'h2' );
+        h2.setAttribute("tabindex",0)
         const h3 = document.createElement( 'h3' )
+        h3.setAttribute("tabindex",0)
         const h4 = document.createElement('h4')
+        h4.setAttribute("tabindex",0)
         const h5 = document.createElement('h5')
+        h5.setAttribute("tabindex",0)
         h2.textContent = name;
         h3.textContent = city +',' + " " + country
         h4.textContent = tagline
