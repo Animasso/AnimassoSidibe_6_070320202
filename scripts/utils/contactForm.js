d702b62137b8d22dca5bc899ffe4532838c5082c
Animasso Sidibe <asidibe017@gmail.com>
2022-04-04 18:35:42 +0200

    ajout des like et du formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,16 @@
+const main = document.getElementById('mainPhotograph')
+const footer = document.querySelector('.like_price')
+console.log(main);
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+    main.style.display = "none"
+    footer.style.display ="none"
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    main.style.display = "block"
+    footer.style.display ="block"
 }
