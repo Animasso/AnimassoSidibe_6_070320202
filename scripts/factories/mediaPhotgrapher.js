7619c2125146c7dae46ba795bd31d39db7c0b2fc
Animasso Sidibe <asidibe017@gmail.com>
2022-04-12 00:04:39 +0200

    display photos

--- scripts/factories/mediaPhotgrapher.js
+++ scripts/factories/mediaPhotgrapher.js
@@ -1,55 +1,68 @@
 function mediaListFactory(data) {
-    const {id,name,date,likes,price,title,image,video,photographerId} = data;
+    const { id, name, date, likes, price, title, image, video, photographerId } = data;
 
     const mediasImages = `assets/photographers/${photographerId}/${image}`;
     const mediasVideos = `assets/photographers/${photographerId}/${video}`
-    const likeheart =`assets/icons/heart.png`
+    const likeheart = `assets/icons/heart.png`
     function mediasCardDOM() {
 
-    
+
         const photosPlusTitle = document.createElement('div')
         photosPlusTitle.classList.add('photos_Plus_Title')
-        const imgMain = document.createElement('img')
-        imgMain.classList.add('portofolio')
-        imgMain.setAttribute('src',mediasImages)
-        imgMain.setAttribute('aria-label','photographe photo')
-        imgMain.setAttribute('tabindex',0)
-        photosPlusTitle.appendChild(imgMain)
-        
+        if (video == undefined) {
+            const imgMain = document.createElement('img')
+            imgMain.classList.add('portofolio')
+            imgMain.setAttribute('src', mediasImages)
+            imgMain.setAttribute('aria-label', 'photographe photo')
+            imgMain.setAttribute('tabindex', 0)
+            photosPlusTitle.appendChild(imgMain)
+        } else {
+            const videoMain = document.createElement('div')
+            videoMain.classList.add('portofolio')
+            const portofolioVideo = document.createElement('video')
+            videoMain.appendChild(portofolioVideo)
+            portofolioVideo.setAttribute('src', mediasVideos,)
+            portofolioVideo.setAttribute('type', 'video/mp4',)
+            portofolioVideo.setAttribute('aria-label', 'photographe video')
+            portofolioVideo.setAttribute('tabindex', 0)
+            photosPlusTitle.appendChild(videoMain)
+        }
+
+
         const photoTitle = document.createElement('div')
         photoTitle.classList.add('photos-title')
         photosPlusTitle.appendChild(photoTitle)
-        
-        
-        const titles= document.createElement('div')
+
+
+        const titles = document.createElement('div')
         titles.classList.add('title')
-        titles.setAttribute('tabindex',0)
+        titles.setAttribute('tabindex', 0)
         titles.textContent = title
         photoTitle.appendChild(titles)
-        
-        const numberLike= document.createElement('div')
+
+        const numberLike = document.createElement('div')
         numberLike.classList.add('numberLike')
-        numberLike.setAttribute('tabindex',0)
-        numberLike.setAttribute('aria-label','nombre de like photo' )
+        numberLike.setAttribute('tabindex', 0)
+        numberLike.setAttribute('aria-label', 'nombre de like photo')
         photoTitle.appendChild(numberLike)
-        
+
         const pLike = document.createElement('p')
         pLike.classList.add('heart')
         pLike.textContent = likes
-        pLike.setAttribute('tabindex',0)
+        pLike.setAttribute('tabindex', 0)
         numberLike.appendChild(pLike)
-        
+
         const iheart = document.createElement('p')
         iheart.classList.add('fa-solid')
         const pLikeHeart = document.createElement('img')
-        pLikeHeart.setAttribute('src',likeheart)
+        pLikeHeart.setAttribute('src', likeheart)
         iheart.appendChild(pLikeHeart)
         numberLike.appendChild(iheart)
 
         return photosPlusTitle
     }
 
-    return { id,name,date,likes,price,title,image,video,photographerId,mediasCardDOM }
+    return { id, name, date, likes, price, title, image, video, photographerId, mediasCardDOM }
 }
 
 
