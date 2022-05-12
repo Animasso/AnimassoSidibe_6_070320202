bf1044dcdbff8b6cd8c1dd2a269b2dd3d2fb1a81
Animasso Sidibe <asidibe017@gmail.com>
2022-05-12 18:06:33 +0200

    formulaire changement

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -41,74 +41,68 @@ closeForm.addEventListener('keydown',(e)=>{
     closeModal()
 }
 })
-function sendForm(){
-  const form =document.getElementById('contact_Photograph')
-  console.log(form);
-      form.addEventListener('submit',(e) => {
-          e.preventDefault();
-      //creation de variable pour stocker les valeurs du formulaire
-          let formIsValid = true;
-          let firstNameValue = firstName.value;
-          let lastNameValue = lastName.value
-          let messageValue = message.value
-          let emailValue = email.value
-          const emailFormat = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}$/i;
-         
-      //gestion erreur premon
-          if ( firstNameValue.length < 2) {
-            firstError.style.display = "block";
-            firstError.style.color ="red"
-            firstName.style.border ="2px solid red"
+const form =document.getElementById('contact_Photograph')
+console.log(form);
+    form.addEventListener('submit',(e) => {
+        e.preventDefault();
+    //creation de variable pour stocker les valeurs du formulaire
+        let formIsValid = true;
+        let firstNameValue = firstName.value;
+        let lastNameValue = lastName.value
+        let messageValue = message.value
+        let emailValue = email.value
+        const emailFormat = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}$/i;
+       
+    //gestion erreur premon
+        if ( firstNameValue.length < 2) {
+          firstError.style.display = "block";
+          firstError.style.color ="red"
+          firstName.style.border ="2px solid red"
+          formIsValid =false
+        }else{
+          firstError.style.display = "none";
+        }
+        console.log(firstNameValue);
+    //gestion erreur nom
+        if ( lastNameValue.length < 2 ) {
+            lastError.style.display = "block";
+            lastError.style.color ="red"
+            lastName.style.border ="2px solid red"
             formIsValid =false
-          }else{
-            firstError.style.display = "none";
-          }
-          console.log(firstNameValue);
-      //gestion erreur nom
-          if ( lastNameValue.length < 2 ) {
-              lastError.style.display = "block";
-              lastError.style.color ="red"
-              lastName.style.border ="2px solid red"
-              formIsValid =false
-          }else{
-            lastError.style.display = "none";
-          }
-      //gestion erreur email
-          if (!emailFormat.test(emailValue)) {
-            emailError.style.display ="block";
-            emailError.style.color ="red"
-            email.style.border ="2px solid red"
-            formIsValid =false
-          }else{
-            emailError.style.display = "none";
-          }
-      //gestion erreur message
-          if (messageValue.length < 4 ) {
-            messageError.style.display ="block"
-            messageError.style.color ="red"
-            message.style.border ="2px solid red"
-            formIsValid = false
-          }else{
-            messageError.style.display = "none";
-          }
-          console.log(lastNameValue);
-          console.log(emailValue);
-          console.log(messageValue);
-      //si il n'y a aucune erreur  la fonction est appelée
-      if(formIsValid === true){
-        sendForm();
-        const modal = document.getElementById("contact_modal");
-        modal.style.display = "none";
-        main.style.display = "block"
-        footer.style.display ="block"
-        lightBox.style.display ='none'
-      }
-        })
-
-
-
-}
-
+        }else{
+          lastError.style.display = "none";
+        }
+    //gestion erreur email
+        if (!emailFormat.test(emailValue)) {
+          emailError.style.display ="block";
+          emailError.style.color ="red"
+          email.style.border ="2px solid red"
+          formIsValid =false
+        }else{
+          emailError.style.display = "none";
+        }
+    //gestion erreur message
+        if (messageValue.length < 4 ) {
+          messageError.style.display ="block"
+          messageError.style.color ="red"
+          message.style.border ="2px solid red"
+          formIsValid = false
+        }else{
+          messageError.style.display = "none";
+        }
+        console.log(lastNameValue);
+        console.log(emailValue);
+        console.log(messageValue);
+    //si il n'y a aucune erreur  la fonction est appelée
+    if(formIsValid === true){
+      sendForm(e);
+      const modal = document.getElementById("contact_modal");
+    modal.style.display = "none";
+    main.style.display = "block"
+    footer.style.display ="block"
+    lightBox.style.display ='none'
+    }
+      })
 
      
 
