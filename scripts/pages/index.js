51708acc26ad162d66fa53eeda380c6423cb6a30
Animasso Sidibe <asidibe017@gmail.com>
2023-04-03 16:44:36 +0200

    changement de chemin

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,26 +1,29 @@
 /* eslint-disable no-undef */
+
 async function getPhotographers() {
-    // récuperation des données des photographes
-    const data = await fetch("https://raw.githubusercontent.com/Animasso/AnimassoSidibe_6_070320202/main/data/photographers.json")
-    .then(response => response.json())
-    const photographers = data.photographers
-    return ({
-        photographers: [...photographers]})
+  // récuperation des données des photographes
+  const data = await fetch("../../data/photographers.json").then((response) =>
+    response.json()
+  );
+  const photographers = data.photographers;
+  return {
+    photographers: [...photographers],
+  };
 }
 
-//affichage des donnees 
+//affichage des donnees
 async function displayData(photographers) {
-    const photographersSection = document.querySelector(".photographer_section");
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
+  const photographersSection = document.querySelector(".photographer_section");
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
 }
 async function init() {
-    // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
 }
 
 init();
